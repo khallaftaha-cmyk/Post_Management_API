a3ac5f7511e1e57638c19b1de04e09e6a28bd094
khallaf taher <khallaftaha759@gmail.com>
2025-12-01 21:53:52 +0100

    added some few modifications to the files

--- app/routers/user.py
+++ app/routers/user.py
@@ -4,12 +4,10 @@ from .. import models, schemas , utils
 from ..database import get_db
 
 
-router = APIRouter(
-    tags = ['Users']
-)
+router = APIRouter()
 
 @router.post("/users",status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_posts(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
